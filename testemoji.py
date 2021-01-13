f2ec881e57c45d5e1fd342f628e69a9532c5e764
River Zhou <riverzhou2000@163.com>
2021-01-13 12:47:12 +0800

    V0.9: Add Test Script

---
+++ testemoji.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import os
+import json
+
+jsonfilename = 'emojis.json'
+testfilename = 'TEST.md'
+
+with open(jsonfilename, 'r', encoding='utf-8') as rf:
+    dictEmoji = json.load(rf)
+
+output = '* Emoji TEST  \n\n---\n'
+for name in dictEmoji:
+    spam = '`:{}:` :{}:'.format(name,name)
+    output += spam
+
+with open(testfilename, 'w', encoding='utf-8') as wf:
+    wf.write(output)
